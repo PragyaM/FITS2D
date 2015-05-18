9a6d69ce9b89abd7a683b41ec63d5650070cbdf5
Pragya Mohan <mohanprag@gmail.com>
2015-05-19 06:53:32 +1200

    Add RGB colouring to images. Not working properly yet.

--- src/models/FitsImage.java
+++ src/models/FitsImage.java
@@ -1,17 +1,48 @@
 package models;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 
 import javafx.scene.image.Image;
+import nom.tam.fits.Fits;
+import nom.tam.fits.FitsException;
+import nom.tam.fits.ImageHDU;
 
 public class FitsImage extends Image{
 	private InputStream is;
+	private ImageHDU hdu;
+	private Fits fitsFile;
 	
 	//TODO handle uncaught exceptions
-	public FitsImage(InputStream is){
+	public FitsImage(InputStream is, Fits fitsFile, ImageHDU hdu){
 		super(is);
 		this.is = is;
+		this.fitsFile = fitsFile;
+		this.hdu = hdu;
+		
+//		printFitsInfo();
 	}
 	
 	//TODO add methods for manipulating FITS image data
+	
+	public void printFitsInfo(){
+		float[][] data = (float[][]) hdu.getKernel();
+		for (int i = 0; i < data.length; i++){
+			for (int j = 0; j < data[i].length; j++){
+				System.out.print(data[i][j] + ", ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Number of HDUs: " + fitsFile.getNumberOfHDUs());
+		System.out.println("Author: " + hdu.getAuthor());
+		try {
+			System.out.println("BitPix" + hdu.getBitPix());
+		} catch (FitsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/services/BuildFitsImage.java
+++ src/services/BuildFitsImage.java
@@ -6,6 +6,8 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import javafx.scene.paint.Color;
+
 import javax.imageio.ImageIO;
 
 import models.FitsImage;
@@ -19,37 +21,108 @@ public class BuildFitsImage {
 	private ImageHDU hdu;
 	private int hduWidth;
 	private int hduHeight;
-	
+
 	//TODO handle uncaught exceptions
 	public BuildFitsImage(Fits fitsFile) throws FitsException, IOException{
 		this.fitsFile = fitsFile;
 	}
-	
+
 	//TODO handle uncaught exceptions
 	public FitsImage call() throws FitsException, IOException{
+		
 		//grab image data from FITS file
+		
 		hdu = (ImageHDU) fitsFile.getHDU(0);
 		hduWidth = hdu.getAxes()[1];
 		hduHeight = hdu.getAxes()[0];
 		
+		System.out.println("bunit: " + hdu.getBUnit());
+
 		//use image tiler to retrieve data for the full image
-		ImageTiler tiler = hdu.getTiler();
-	    float[] img = (float[]) tiler.getTile(new int[]{0, 0}, hdu.getAxes());
-	    
-	    //write image data
-	    BufferedImage im = new BufferedImage(hduWidth, hduHeight, BufferedImage.TYPE_BYTE_BINARY);
-		WritableRaster raster = im.getRaster();
-		raster.setPixels(0, 0, hduWidth, hduHeight, img);
 		
+		ImageTiler tiler = hdu.getTiler();
+		float[] img = (float[]) tiler.getTile(new int[]{0, 0}, hdu.getAxes());
+
+		//write image data
+
+		BufferedImage im = new BufferedImage(hduWidth, hduHeight, BufferedImage.TYPE_3BYTE_BGR);
+		WritableRaster raster = im.getRaster();
+		
+		double max = hdu.getMaximumValue();
+		double cutOff = max * 0.75;
+		
+		int colBandRange = 16777215;
+		float segmentSize = (float) (cutOff/colBandRange);
+
+		for (int i = 0; i < img.length; i++){
+			float val = (float) (img[i]);
+			int x = i % hduWidth;
+			int y = i / hduWidth;
+			if (val == Double.NaN || val <= 0) {
+				raster.setSample(x, y, 0, 0);
+				raster.setSample(x, y, 1, 0);
+				raster.setSample(x, y, 2, 0);
+			} 
+			else if (val > cutOff){
+				raster.setSample(x, y, 0, 255);
+				raster.setSample(x, y, 1, 255);
+				raster.setSample(x, y, 2, 255);
+			}
+			else {
+				float colVal = val/segmentSize;
+				Color c = getColorFromFloat(colVal);
+				raster.setSample(x, y, 0, c.getRed()*256);
+				raster.setSample(x, y, 1, c.getGreen()*256);
+				raster.setSample(x, y, 2, c.getBlue()*256);
+				
+				//Old hacky code
+//				if (val < 10){
+//					raster.setSample(x, y, 0, colVal*20);
+//					raster.setSample(x, y, 1, colVal*10);
+//					raster.setSample(x, y, 2, colVal);
+//				}
+//				else if (val < 60){
+//					raster.setSample(x, y, 0, colVal*85);
+//					raster.setSample(x, y, 1, colVal*90);
+//					raster.setSample(x, y, 2, colVal);
+//				}
+//				else if (val <= (int) hdu.getMaximumValue() + offset){
+//					raster.setSample(x, y, 0, colVal*20);
+//					raster.setSample(x, y, 1, colVal*2);
+//					raster.setSample(x, y, 2, colVal*90);
+//				}
+//				else {
+//					raster.setSample(x, y, 0, 0);
+//					raster.setSample(x, y, 1, 0);
+//					raster.setSample(x, y, 2, 0);
+//				}
+			}
+		}
+
 		//convert image to format that can be displayed
+		
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		ImageIO.write(im, "png", out);
 		out.flush();
 		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
-		
-		FitsImage fitsImage = new FitsImage(in);
+
+		FitsImage fitsImage = new FitsImage(in, fitsFile, hdu);
 		//TODO: set properties of fitsImage
-		
+
 		return fitsImage;
+	}
+	
+	public Color getColorFromFloat(float fVal){
+		int red = 0;
+		int green = 0;
+		int blue = 0;
+		
+		blue = (int) Math.floor(fVal / 256 / 256 );
+		green = (int) Math.floor((fVal - blue * 256 * 256) / 256);
+		red = (int) Math.floor(fVal - blue * 256 * 256 - green * 256);
+
+		Color c = Color.rgb(red, green, blue);
+		
+		return c;
 	}
 }
