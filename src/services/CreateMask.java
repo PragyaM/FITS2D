db9c93b16218b9c3f2a4017f962c4675e34754a8
Pragya Mohan <mohanprag@gmail.com>
2015-06-25 10:39:19 +1200

    Handle creating mask with base data type other than float

--- src/services/CreateMask.java
+++ src/services/CreateMask.java
@@ -8,6 +8,7 @@ import models.FitsImage;
 import nom.tam.fits.Fits;
 import nom.tam.fits.FitsException;
 import nom.tam.fits.FitsFactory;
+import nom.tam.fits.Header;
 import nom.tam.util.ArrayFuncs;
 
 public class CreateMask {
@@ -15,22 +16,46 @@ public class CreateMask {
 	public static Fits mapToFits(ArrayList<Point> maskPoints, FitsImage fitsImage, int cWidth, int cHeight) throws FitsException, IOException {
 		//Get corresponding values from FITS data
 		Fits maskFits = new Fits();
-
 		maskFits.addHDU(fitsImage.getHDU());
-
 		double[][] oldData = (double[][]) ArrayFuncs.convertArray(fitsImage.getHDU().getKernel(), double.class);
-
 		double[][] newData = new double[oldData.length][oldData[0].length];
 		newData = setAll(newData, Double.NaN);
 		newData = setMaskValues(newData, maskPoints, fitsImage, cWidth, cHeight);
-
-		maskFits.addHDU(FitsFactory.HDUFactory(newData));
-
-
-		double[][] img = (double[][]) ArrayFuncs.convertArray(maskFits.getHDU(0).getKernel(), double.class);
-		for (int i=0; i<img.length; i += 1) {
-			for (int j=0; j<img[i].length; j += 1) {
-				img[i][j] = newData[i][j];
+		
+		//get base type of original data
+		Class type = ArrayFuncs.getBaseClass(fitsImage.getHDU().getKernel());
+		maskFits.addHDU(FitsFactory.HDUFactory(ArrayFuncs.convertArray(newData, type)));
+		
+		if (type == float.class){
+			float[][] floatArray = (float[][]) maskFits.getHDU(0).getKernel();
+			for (int i=0; i<floatArray.length; i += 1) {
+				for (int j=0; j<floatArray[i].length; j += 1) {
+					floatArray[i][j] = (float) newData[i][j];
+				}
+			}
+		}
+		else if (type == double.class){
+			double[][] doubleArray = (double[][]) maskFits.getHDU(0).getKernel();
+			for (int i=0; i<doubleArray.length; i += 1) {
+				for (int j=0; j<doubleArray[i].length; j += 1) {
+					doubleArray[i][j] = (double) newData[i][j];
+				}
+			}
+		}
+		else if (type == short.class){
+			short[][] shortArray = (short[][]) maskFits.getHDU(0).getKernel();
+			for (int i=0; i<shortArray.length; i += 1) {
+				for (int j=0; j<shortArray[i].length; j += 1) {
+					shortArray[i][j] = (short) newData[i][j];
+				}
+			}
+		}
+		else if (type == long.class){
+			long[][] longArray = (long[][]) maskFits.getHDU(0).getKernel();
+			for (int i=0; i<longArray.length; i += 1) {
+				for (int j=0; j<longArray[i].length; j += 1) {
+					longArray[i][j] = (long) newData[i][j];
+				}
 			}
 		}
 
@@ -38,11 +63,13 @@ public class CreateMask {
 	}
 
 	private static double[][] setAll(double[][] data, double value){
+		
 		for (int i=0; i<data.length; i++){
 			for (int j=0; j<data[i].length; j++){
 				data[i][j] = value;
 			}
 		}
+		
 		return data;
 	}
 
