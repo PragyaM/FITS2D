b9eb68587dc1ccc0fc6de1b498edcf4b85648d08
Pragya Mohan <mohanprag@gmail.com>
2015-07-16 12:22:52 +1200

    Fix fill algorithm to stop if colour of clicked pixel is the same as the replacement colour

--- src/services/DoFill.java
+++ src/services/DoFill.java
@@ -20,8 +20,10 @@ public class DoFill extends Task<ArrayList<Point>>{
 	private final int target = 0;
 	private final ArrayList<Point> points;
 	private final int[][] canvasState;
-	private final int replacement = 1;
+	private final int replacement;
 	private final Color replacementColor;
+	private final Color targetColor;
+	private final PixelReader pRead;
 	
 	public DoFill(Canvas c, Point orig, Color replaceCol){
 		origin = orig;
@@ -33,10 +35,19 @@ public class DoFill extends Task<ArrayList<Point>>{
 		width = (int) canvas.getWidth();
 		height = (int) canvas.getHeight();
 		wImg = new WritableImage(width, height); //FIXME: doesn't take zoom into account
+		SnapshotParameters sp = new SnapshotParameters();
+	    sp.setFill(Color.TRANSPARENT);
+		canvas.snapshot(sp, wImg);
+		pRead = wImg.getPixelReader();
+		targetColor = pRead.getColor(origin.x, origin.y);
+		if (targetColor.equals(replacementColor)){
+			replacement = target;
+		} else replacement = 1;
+		
 		canvasState = getCanvasState();
 	}
 	
-	private ArrayList<Point> doFill(Point node, int target, int replacement, ArrayList<Point> points){
+	private ArrayList<Point> doFill(Point node, ArrayList<Point> points){
 		if (target == replacement){
 			return points;
 		}
@@ -49,26 +60,19 @@ public class DoFill extends Task<ArrayList<Point>>{
 		canvasState[node.x][node.y] = replacement;
 		points.add(node);
 		
-		doFill(new Point(node.x-1, node.y), target, replacement, points);
-		doFill(new Point(node.x+1, node.y), target, replacement, points);
-		doFill(new Point(node.x, node.y-1), target, replacement, points);
-		doFill(new Point(node.x, node.y+1), target, replacement, points);
+		doFill(new Point(node.x-1, node.y), points);
+		doFill(new Point(node.x+1, node.y), points);
+		doFill(new Point(node.x, node.y-1), points);
+		doFill(new Point(node.x, node.y+1), points);
 		return points;
 	}
 	
 	private int[][] getCanvasState(){
-		SnapshotParameters sp = new SnapshotParameters();
-	    sp.setFill(Color.TRANSPARENT);
-		canvas.snapshot(sp, wImg);
-		PixelReader pRead = wImg.getPixelReader();
-		Color targetColor = pRead.getColor(origin.x, origin.y);
+		
 		int[][] cState = new int[width][height];
 		for (int x = 0; x < width; x++){
 			for (int y = 0; y<height; y++){
-				if (pRead.getColor(x, y).equals(targetColor)){
-					cState[x][y] = target;
-				}
-				else if (pRead.getColor(x, y).equals(replacementColor)){
+				if (pRead.getColor(x, y).equals(replacementColor)){
 					cState[x][y] = replacement;
 				}
 				else cState[x][y] = target;
@@ -83,7 +87,7 @@ public class DoFill extends Task<ArrayList<Point>>{
 
 	@Override
 	protected ArrayList<Point> call() throws Exception {
-		return doFill(origin, target, replacement, points);
+		return doFill(origin, points);
 	}
 
 }
