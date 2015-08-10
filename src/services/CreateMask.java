c0971c05258be373a6e0d5888c03b9cf7184f41f
Pragya Mohan <mohanprag@gmail.com>
2015-08-10 16:24:40 +1200

    Task 8: Crop new FITS files to bound the selected region from which they are being generated

--- src/services/CreateMask.java
+++ src/services/CreateMask.java
@@ -4,71 +4,66 @@ import java.awt.Point;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javafx.scene.shape.Rectangle;
 import models.FitsImage;
 import nom.tam.fits.Fits;
 import nom.tam.fits.FitsException;
-import nom.tam.fits.FitsFactory;
-import nom.tam.fits.Header;
 import nom.tam.util.ArrayFuncs;
 
 public class CreateMask {
 
+	private static Rectangle boundingBox;
+
 	public static Fits mapToFits(ArrayList<Point> maskPoints, FitsImage fitsImage, int cWidth, int cHeight) throws FitsException, IOException {
 		//Get corresponding values from FITS data
 		Fits maskFits = new Fits();
-		maskFits.addHDU(fitsImage.getHDU());
-		double[][] oldData = (double[][]) ArrayFuncs.convertArray(fitsImage.getHDU().getKernel(), double.class);
-		double[][] newData = new double[oldData.length][oldData[0].length];
+
+		boundingBox = getBoundingBox(fitsImage, maskPoints, cWidth, cHeight);
+		System.out.println("Region size: " + boundingBox.getWidth() + ", " + boundingBox.getHeight());
+		System.out.println("Region position: " + boundingBox.getX() + ", " + boundingBox.getY());
+
+		double[][] newData = new double[(int) boundingBox.getHeight()][(int) boundingBox.getWidth()];
 		newData = setAll(newData, Double.NaN);
 		newData = setMaskValues(newData, maskPoints, fitsImage, cWidth, cHeight);
-		
+
 		//get base type of original data
 		Class type = ArrayFuncs.getBaseClass(fitsImage.getHDU().getKernel());
-		
-		if (type == float.class){
-			float[][] floatArray = (float[][]) maskFits.getHDU(0).getKernel();
-			for (int i=0; i<floatArray.length; i += 1) {
-				for (int j=0; j<floatArray[i].length; j += 1) {
-					floatArray[i][j] = (float) newData[i][j];
-				}
-			}
-		}
-		else if (type == double.class){
-			double[][] doubleArray = (double[][]) maskFits.getHDU(0).getKernel();
-			for (int i=0; i<doubleArray.length; i += 1) {
-				for (int j=0; j<doubleArray[i].length; j += 1) {
-					doubleArray[i][j] = (double) newData[i][j];
-				}
-			}
-		}
-		else if (type == short.class){
-			short[][] shortArray = (short[][]) maskFits.getHDU(0).getKernel();
-			for (int i=0; i<shortArray.length; i += 1) {
-				for (int j=0; j<shortArray[i].length; j += 1) {
-					shortArray[i][j] = (short) newData[i][j];
-				}
-			}
-		}
-		else if (type == long.class){
-			long[][] longArray = (long[][]) maskFits.getHDU(0).getKernel();
-			for (int i=0; i<longArray.length; i += 1) {
-				for (int j=0; j<longArray[i].length; j += 1) {
-					longArray[i][j] = (long) newData[i][j];
-				}
-			}
-		}
+
+		Object data = newData;
+		ArrayFuncs.convertArray(data, type);
+		maskFits.addHDU(Fits.makeHDU(data));
 
 		return maskFits;
 	}
 
+	private static Rectangle getBoundingBox(FitsImage fitsImage, ArrayList<Point> maskPoints, int width, int height){
+		int minX = Integer.MAX_VALUE;
+		int maxX = 0;
+		int minY = Integer.MAX_VALUE;
+		int maxY = 0;
+
+		for (Point p : maskPoints){
+			ArrayList<Point> dataPoints = fitsImage.getDataPositions(p, width, height);
+			for (Point dataPoint : dataPoints){
+				if (dataPoint.x < minX) minX = dataPoint.x;
+				if (dataPoint.x > maxX) maxX = dataPoint.x;
+				if (dataPoint.y < minY) minY = dataPoint.y;
+				if (dataPoint.y > maxY) maxY = dataPoint.y;
+			}
+
+		}
+
+		return new Rectangle(minX, minY, maxX-minX + 1, maxY-minY + 1);
+	}
+
 	private static double[][] setAll(double[][] data, double value){
-		
+
 		for (int i=0; i<data.length; i++){
 			for (int j=0; j<data[i].length; j++){
 				data[i][j] = value;
 			}
 		}
-		
+
 		return data;
 	}
 
@@ -77,7 +72,11 @@ public class CreateMask {
 		for (Point p : maskPoints){
 			ArrayList<Point> dataPositions = fitsImage.getDataPositions(p, cw, ch);
 			for (Point dataPos : dataPositions){
-				data[dataPos.y][dataPos.x] = fitsImage.getValueAt(dataPos);
+				try {
+					data[dataPos.y - (int) boundingBox.getY()][dataPos.x - (int) boundingBox.getX()] = fitsImage.getValueAt(dataPos);
+				} catch (Exception e) {
+					System.out.println(e.getMessage() + " out of bounds in (" + (dataPos.y - (int) boundingBox.getY()) + ", " + (dataPos.x - (int) boundingBox.getX()) + ")");
+				}
 			}
 		}
 
