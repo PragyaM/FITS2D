b1e455517e5003e3c7864745c918ab4d204cc2e8
Pragya Mohan <mohanprag@gmail.com>
2015-08-10 16:18:13 +1200

    Task 8: Boundary check for positional data retrieval. Also, code formatting adjustments.

--- src/models/FitsImage.java
+++ src/models/FitsImage.java
@@ -7,6 +7,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
@@ -26,14 +27,14 @@ public class FitsImage{
 	private StandardImageTiler tiler;
 	private int width;
 	private int height;
-	
+
 	private double[] imageFriendlyData;
 	private Color nanColour;
-	
+
 	private double[][] processingFriendlyData;
 	private Image image;
 	private GUIController controller;
-	
+
 	//TODO handle uncaught exceptions
 	public FitsImage(Fits fitsFile, GUIController controller) throws FitsException, IOException{
 		this.controller = controller;
@@ -46,9 +47,9 @@ public class FitsImage{
 		prepareData();
 		writeImage();
 	}
-	
+
 	private void prepareData(){
-		
+
 		double[] img = null;
 		try {
 			Object dataArray = tiler.getTile(new int[]{0, 0}, hdu.getAxes());
@@ -63,35 +64,37 @@ public class FitsImage{
 		} catch (IOException | FitsException e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println(processingFriendlyData.length + ", " + processingFriendlyData[0].length);
 	}
-	
+
 	public ArrayList<Point> getDataPositions(Point p, int cWidth, int cHeight){
-		ArrayList<Point> points = new ArrayList<Point>();
+		HashSet<Point> points = new HashSet<Point>();
 		int x0 = (p.x * width)/cWidth;
 		int y0 = (p.y * height)/cHeight;
-		
+
 		int x1 = x0 + width/cWidth;
 		int y1 = y0 + height/cHeight;
-		
+
 		for (int x = x0; x <= x1; x++){
 			for (int y = y0; y <= y1; y++){
 				Point pos = new Point(x, height - y);
 				points.add(pos);
 			}
 		}
-		return points;
+		return (new ArrayList<Point>(points));
 	}
-	
+
 	public double getValueAt(Point p){
-		return processingFriendlyData[p.y][p.x];
+		if (p.x < width && p.y < height && p.x >= 0 && p.y >= 0){
+			return processingFriendlyData[p.y][p.x];
+		} else return processingFriendlyData[0][0];
 	}
-	
+
 	public Fits getFitsFile(){
 		return fitsFile;
 	}
-	
+
 	public Image getImage(){
 		return image;
 	}
@@ -99,20 +102,20 @@ public class FitsImage{
 	public double[][] getData(){
 		return processingFriendlyData;
 	}
-	
+
 	public ImageHDU getHDU(){
 		return hdu;
 	}
-	
+
 	//SETUP METHODS:
-	
+
 	public void writeImage() throws IOException{
 		//write image data
 		BufferedImage im = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		
+
 		WritableRaster raster = im.getRaster();
 		setImageColours(imageFriendlyData, raster);
-		
+
 		//convert image to a format that can be displayed
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		ImageIO.write(im, "png", out);
@@ -121,15 +124,15 @@ public class FitsImage{
 
 		image = new Image(in);
 	}
-	
+
 	public void setNanColour(Color replacement) throws IOException{
 		nanColour = replacement;
 	}
-	
+
 	private void setImageColours(double[] imageData, WritableRaster raster){
 		double max = hdu.getMaximumValue();
 		double cutOff = max * 0.025;
-		
+
 		int colBandRange = 250;
 		double segmentSize = (double) (cutOff/colBandRange);
 
@@ -149,11 +152,11 @@ public class FitsImage{
 			}
 		}
 	}
-	
+
 	private boolean isNaN(double d){
 		return d != d;
 	}
-	
+
 	/**
 	 * @param wavelength - expected to be within the 380 to 780 range (where visible colors can be resolved).
 	 * @return a Color containing calculated RGB values
@@ -163,70 +166,70 @@ public class FitsImage{
 		double green = 0;
 		double blue = 0;
 		double SSS;
-		
+
 		if (wavelength < 380){
 			red = 1.0;
-	        green = 1.0;
-	        blue = 1.0;
+			green = 1.0;
+			blue = 1.0;
 		}
 		if (wavelength >= 380 && wavelength < 440){
 			red = -(wavelength - 440) / (440 - 350);
-	        green = 0.1; //0.0;
-	        blue = 0.9; //1.0;
+			green = 0.1; //0.0;
+			blue = 0.9; //1.0;
 		}
 		else if (wavelength >= 440 && wavelength < 490){
 			red = 0.1; //0.0;
-	        green = (wavelength - 440) / (490 - 440);
-	        blue = 0.8; //1.0;
+			green = (wavelength - 440) / (490 - 440);
+			blue = 0.8; //1.0;
 		}
 		else if (wavelength >= 490 && wavelength < 510){
 			red = 0.1; //0.0;
-	        green = 0.9; //1.0;
-	        blue = -(wavelength - 510) / (510 - 490);
+			green = 0.9; //1.0;
+			blue = -(wavelength - 510) / (510 - 490);
 		}
 		else if (wavelength >= 510 && wavelength < 580){
 			red = (wavelength - 510) / (580 - 510);
-	        green = 0.8; //1.0;
-	        blue = 0.2;
+			green = 0.8; //1.0;
+			blue = 0.2;
 		}
 		else if (wavelength >= 580 && wavelength < 645){
 			red = 1.0; //1.0;
-	        green = -(wavelength - 645) / (645 - 580);
-	        blue = 0.3; //0.0;
+			green = -(wavelength - 645) / (645 - 580);
+			blue = 0.3; //0.0;
 		}
 		else if (wavelength >= 645 && wavelength <= 780){
 			red = 1.0; //1.0;
-	        green = 0.6;  //0.0;
-	        blue = 0.4; //0.0;
+			green = 0.6;  //0.0;
+			blue = 0.4; //0.0;
 		}
-	    else{
-	    	red = 0.0;
-	        green = 0.0;
-	        blue = 0.0;
-	    }
+		else{
+			red = 0.0;
+			green = 0.0;
+			blue = 0.0;
+		}
 
-	    // intensity correction
-	    if (wavelength >= 380 && wavelength < 420){
-	    	SSS = 0.3 + 0.7*(wavelength - 350) / (420 - 350);
-	    }
-	    else if (wavelength >= 420 && wavelength <= 700){
-	    	SSS = 1.0;
-	    } 
-	    else if (wavelength > 700 && wavelength <= 780){
-	    	SSS = 0.3 + 0.7*(780 - wavelength) / (780 - 700);
-	    } 
-	    else{
-	    	SSS = 0.0;
-	    }
-	    SSS *= 255;
+		// intensity correction
+		if (wavelength >= 380 && wavelength < 420){
+			SSS = 0.3 + 0.7*(wavelength - 350) / (420 - 350);
+		}
+		else if (wavelength >= 420 && wavelength <= 700){
+			SSS = 1.0;
+		} 
+		else if (wavelength > 700 && wavelength <= 780){
+			SSS = 0.3 + 0.7*(780 - wavelength) / (780 - 700);
+		} 
+		else{
+			SSS = 0.0;
+		}
+		SSS *= 255;
 
-	    Color c = Color.rgb((int)(SSS*red), (int)(SSS*green), (int)(SSS*blue));
-		
+		Color c = Color.rgb((int)(SSS*red), (int)(SSS*green), (int)(SSS*blue));
+
 		return c;
 	}
-	
+
 	//TODO add methods for manipulating FITS image data
-	
+
 	public void printFitsInfo(){
 		float[][] data = (float[][]) hdu.getKernel();
 		for (int i = 0; i < data.length; i++){
@@ -235,7 +238,7 @@ public class FitsImage{
 			}
 			System.out.println();
 		}
-		
+
 		System.out.println("Number of HDUs: " + fitsFile.getNumberOfHDUs());
 		System.out.println("Author: " + hdu.getAuthor());
 		try {
