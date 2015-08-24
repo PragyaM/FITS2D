93602317b475112d99095d5c1118bad78b7a07c1
Pragya Mohan <mohanprag@gmail.com>
2015-08-25 11:27:44 +1200

    task 13: add images to buttons and a list view to be used for naming and distinguishing annotations within the program

--- src/views/tools_area/AnnotationToolBox.java
+++ src/views/tools_area/AnnotationToolBox.java
@@ -1,29 +1,52 @@
 package views.tools_area;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import controllers.GUIController;
 
 public class AnnotationToolBox extends BaseToolBox{
 	
+	private ListView<String> annotationsList = new ListView<String>();
+    private ObservableList<String> items = FXCollections.observableArrayList();
+	
 	public AnnotationToolBox(GUIController controller, ToggleGroup group){
 		super("Annotation Tools");
 		
-        ToggleButton drawToolButton = new ToggleButton("Annotation Draw Tool");
-        ToggleButton fillToolButton = new ToggleButton("Annotation Bucket Tool");
+		Image imageDraw = new Image(getClass().getResourceAsStream("/resources/pencil112.png"));
+		imageDraw.isPreserveRatio();
+        ToggleButton drawToolButton = new ToggleButton(null, new ImageView(imageDraw));
+        
+        Image imageFill = new Image(getClass().getResourceAsStream("/resources/paint3.png"));
+        imageFill.isPreserveRatio();
+        ToggleButton fillToolButton = new ToggleButton(null, new ImageView(imageFill));
 
         drawToolButton.setToggleGroup(group);
         fillToolButton.setToggleGroup(group);
         
         this.add(drawToolButton, 0, 1);
-        this.add(fillToolButton, 1, 1);
+        this.add(fillToolButton, 0, 2);
         
         drawToolButton.setOnAction(controller.getAnnotationsController().toggleDrawMode(drawToolButton));
         fillToolButton.setOnAction(controller.getAnnotationsController().toggleFillMode(fillToolButton));
         
-        CheckBox hideAnnotationsButton = new CheckBox("Hide Annotations");
-        add(hideAnnotationsButton, 0, 2);
+        CheckBox hideAnnotationsButton = new CheckBox("Hide all");
+        add(hideAnnotationsButton, 0, 3);
         hideAnnotationsButton.setOnAction(controller.getAnnotationsController().toggleAnnotationsVisible(hideAnnotationsButton));
+        
+        addAnnotationItem("Test");
+        annotationsList.autosize();
+        annotationsList.setPrefHeight(100);
+        this.add(annotationsList, 1,  2);
+	}
+	
+	public void addAnnotationItem(String annotationName){
+		items.add(annotationName);
+		annotationsList.setItems(items);
 	}
 }
